9b73244f1b396f45f9393cbe5c8bb5e8ae391795
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-04-18 14:53:11 -0300

    bottuns 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,13 @@ import './Button.css';
 
 export const Button = (props) => {
    return(
-        <button className='button'>{props.label}</button>
+        <button className={`
+            button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+        `}
+         onClick={(e) => props.click && props.click(e.target.innerHTML)}
+        >{props.label}</button>
    );
 }
